59e6e6bd586f29d049daed83f683db21dca16b7f
Tommy Otzen <tommy_otzen@hotmail.com>
2015-04-22 11:06:15 +0200

    change server message
    When a client is logged on, the server should write ‘Connected to Blink’

--- server.js
+++ server.js
@@ -8,30 +8,18 @@ var app = express();
 
 var server = http.createServer(function(request, response){						// Here we pass an anonymous function, that decide what to do on connection
 	console.log('Connection');													// On every connect/response interaction with our server, write 'Connection' in cmd line
-	var path = url.parse(request.url).pathname;									// Here we add other paths, than root ex. localhost:8001/some_other_path 
-	
+	var path = url.parse(request.url).pathname;									// Here we add other paths, than root ex. localhost:8001/some_other_path
+
 	switch(path){																// decide what the server should do, depending on which path we use.
 		case'/':
 			response.writeHead(200, {'Content-Type': 'text/html'});				// Here we define what data-type to use, when writing our response header
 			response.write('Hello World');										// Here we write the actual response to the header, more precisely our html message
 			response.end();
 			break;
-		case '/arduino/digital/13/0':
-			response.writeHead(200, {'Content-Type': 'text/html'});				// Here we define what data-type to use, when writing our response header
-			response.write('Led 13 is off');									// Here we write the actual response to the header, more precisely our html message
-			console.log('Led 13 is off');
-			response.end();
-			break;
-		case '/arduino/digital/13/1':
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write('Led 13 is on');
-			console.log('Led 13 is on');
-			response.end();
-			break;
 		case '/blink':
 			response.writeHead(200, {'Content-Type': 'text/html'});
 			response.write('1');
-			console.log('1');
+			console.log('Connected to Blink');
 			response.end();
 			break;
 		default:
@@ -40,14 +28,14 @@ var server = http.createServer(function(request, response){						// Here we pass
 			response.end();														// Here we end the response and action, and its also here we execute the response!
 			break;
 	}
-															
+
 });
 
 //server.listen(8080);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
+
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
